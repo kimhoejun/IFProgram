1b004a9fa2bc95fcffb48803d8c67e57b80fce4e
kimhoejun <hoijunkim1203@gmail.com>
2019-12-04 23:23:19 +0900

    ModelAndView를 이용한 화면 호출 정의

--- CIS/src/main/java/com/common/WorldControler.java
+++ CIS/src/main/java/com/common/WorldControler.java
@@ -2,14 +2,20 @@ package com.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WorldControler {
 
 	@RequestMapping("/world")
-	public String word() {
+	public ModelAndView word() {
 		
-		return "/world";
+		ModelAndView view = new ModelAndView();
+		
+		view.setViewName("world");
+		
+		return view;
+//		return "/world";
 	}
 
 }
