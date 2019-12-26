46a3900aaac9dfaa172cecc71edc1f8bb573cfaa
kimhoejun <hoijunkim1203@gmail.com>
2019-12-27 01:45:04 +0900

    First..._20191227

--- CIS/src/main/java/com/ru/asnew/DistributedService.java
+++ CIS/src/main/java/com/ru/asnew/DistributedService.java
@@ -1,5 +1,22 @@
 package com.ru.asnew;
 
-public class DistributedService {
+import java.util.Map;
 
+public class DistributedService {
+	
+	private Map<String, Object> headerMap;
+	private Map<String, Object> indataMap;
+
+	public DistributedService(Map<String, Object> jsonMap) {
+		
+		super();
+		this.indataMap = jsonMap;
+	}
+	
+	public Map<String, Object> executeRuleCall() {
+		
+		Map<String, Object> retValue = null;
+		
+		return retValue;
+	}
 }
