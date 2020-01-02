6dd1266185cbe71686502fff20415f4cb63f811b
kimhoejun <hoijunkim1203@gmail.com>
2020-01-03 01:21:02 +0900

    거래구분코드 01일시 회원평점 실행부 정의

--- CIS/src/main/java/com/ru/asnew/DistributedService.java
+++ CIS/src/main/java/com/ru/asnew/DistributedService.java
@@ -2,21 +2,56 @@ package com.ru.asnew;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 public class DistributedService {
 	
-	private Map<String, Object> headerMap;
-	private Map<String, Object> indataMap;
-
+	private static final Logger log = Logger.getLogger(DistributedService.class);
+	
+	private static String TRNS_CLSS = "";
+	private Map<String, Object> jsonMapData;
+	
+	
 	public DistributedService(Map<String, Object> jsonMap) {
 		
 		super();
-		this.indataMap = jsonMap;
+		this.jsonMapData = jsonMap;
 	}
 	
-	public Map<String, Object> executeRuleCall() {
+	
+	public Map<String, Object> executeServiceCall() {
 		
 		Map<String, Object> retValue = null;
 		
+		// 거래구분 코드 정의 (Host에서 전달하는 JsonMap 데이터중, 거래구분 코드를 구분하여 서비스 분리 실행하기 위해)
+		TRNS_CLSS = jsonMapData.get("").toString().trim();
+		
+		if(TRNS_CLSS.length() == 1) {
+			
+			TRNS_CLSS = "0" + TRNS_CLSS;
+		}
+		
+		try {
+			
+			// 회원평점 서비스 구분
+			if(TRNS_CLSS.equals("01") || TRNS_CLSS.equals("01")) {
+				
+				ServiceFnBDScor bdScor = new ServiceFnBDScor(jsonMapData);
+				retValue = bdScor.executeCallBDScorService();
+			}
+			else {
+				
+				// KSException 
+				throw new Exception();
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		
 		return retValue;
 	}
+	
+	
 }
