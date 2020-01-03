23093687956105849af8535af1eb735557722761
kimhoejun <hoijunkim1203@gmail.com>
2020-01-04 00:57:02 +0900

    거래구분 코드에 다른 서비스 분기 기능 추가_20200104

--- CIS/src/main/java/com/ru/asnew/DistributedService.java
+++ CIS/src/main/java/com/ru/asnew/DistributedService.java
@@ -1,5 +1,6 @@
 package com.ru.asnew;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -8,7 +9,8 @@ public class DistributedService {
 	
 	private static final Logger log = Logger.getLogger(DistributedService.class);
 	
-	private static String TRNS_CLSS = "";
+	private static String TRNS_CLSS;	//	Host에 전달받은 jsonMap 형태의 데이터중 거래구분 코드 값 변수 정의
+	
 	private Map<String, Object> jsonMapData;
 	
 	
@@ -18,10 +20,14 @@ public class DistributedService {
 		this.jsonMapData = jsonMap;
 	}
 	
-	
+	//	거래구분 코드에 따른 서비스 분기 정의
 	public Map<String, Object> executeServiceCall() {
 		
-		Map<String, Object> retValue = null;
+		log.info("=========================================================");
+		log.info("executeServiceCall interface [ " + "(I/F ID)" + " ]");
+		log.info("=========================================================");
+		
+		Map<String, Object> retValue = new LinkedHashMap<String, Object>();
 		
 		// 거래구분 코드 정의 (Host에서 전달하는 JsonMap 데이터중, 거래구분 코드를 구분하여 서비스 분리 실행하기 위해)
 		TRNS_CLSS = jsonMapData.get("").toString().trim();
@@ -37,7 +43,8 @@ public class DistributedService {
 			if(TRNS_CLSS.equals("01") || TRNS_CLSS.equals("01")) {
 				
 				ServiceFnBDScor bdScor = new ServiceFnBDScor(jsonMapData);
-				retValue = bdScor.executeCallBDScorService();
+				retValue.put("DATA", bdScor.executeCallBDScorService());
+				
 			}
 			else {
 				
@@ -52,6 +59,5 @@ public class DistributedService {
 		
 		return retValue;
 	}
-	
 	
 }
